e5d7527e69d2242c10849dcbe15607629d305e51
cuongdn <cuongdn@zinza.com.vn>
2023-04-21 13:41:51 +0700

    wait-for-it

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 set -e
-# Database migration
-/myapp/bin/rake db:migrate
 
 # Remove a potentially pre-existing server.pid for Rails.
-rm -f /myapp/tmp/pids/server.pid
+rm -f /app/tmp/pids/server.pid
+
+# Check if the database is ready before migrating
+/usr/bin/wait-for-it.sh db:3306 -- echo "Database is up"
+bundle exec rails db:migrate 2>/dev/null || bundle exec rails db:create db:migrate
 
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
 exec "$@"
