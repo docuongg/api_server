56e6a77edc47c4dbfdf512b86e56e2aa377ef6af
docuongg <dncuong168@gmail.com>
2023-04-27 07:53:47 +0700

    edit-docker-compose

--- app/jobs/create_random_orders_job.rb
+++ app/jobs/create_random_orders_job.rb
@@ -1,17 +1,16 @@
 require 'faker'
 
 class CreateRandomOrdersJob < ApplicationJob
-
-  def perform
+  def perform(*args)
     num_orders = rand(1..5)
 
     (1..num_orders).each do |id|
       Order.create!(
-          description: Faker::Movie.quote,
-          total_price: rand(200..400),
-          status: rand(0..4),
-          user_id: rand(1..10),
-          # created_at: Faker::Time.between(from: 1.months.ago, to: Time.now),
+        description: Faker::Movie.quote,
+        total_price: rand(200..400),
+        status: rand(0..4),
+        user_id: rand(1..10),
+        created_at: Faker::Time.between(from: 1.months.ago, to: Time.now),
       )
     end
   end
