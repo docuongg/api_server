74d6aa68e42b9ebf07b9d63c2cdf60946e0064ea
cuongdn <cuongdn@zinza.com.vn>
2023-04-27 12:12:32 +0700

    schedule-sidekiq

--- Gemfile
+++ Gemfile
@@ -31,3 +31,4 @@ gem "image_processing"
 gem 'ruby-vips'
 gem 'active_model_serializers'
 gem 'sidekiq'
+gem 'sidekiq-scheduler', '~> 5.0', '>= 5.0.2'
--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -6,3 +6,12 @@ end
 Sidekiq.configure_client do |config|
   config.redis = { url: 'redis://redis:6379' }
 end
+
+require 'sidekiq-scheduler'
+
+Sidekiq.configure_server do |config|
+  config.on(:startup) do
+    Sidekiq.schedule = YAML.load_file(File.expand_path('../../../config/sidekiq.yml', __FILE__))
+    Sidekiq::Scheduler.reload_schedule!
+  end
+end
